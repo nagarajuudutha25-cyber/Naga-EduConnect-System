26c84ac8b9861a9735589b6d2df6c7d99d222687
nagarajuudutha25-cyber <dummy@gmail.com>
2026-01-20 02:51:42 +0000

    solve initial commit

--- server/src/main/java/com/edutech/progressive/controller/StudentController.java
+++ server/src/main/java/com/edutech/progressive/controller/StudentController.java
@@ -2,39 +2,46 @@ package com.edutech.progressive.controller;
 
 import com.edutech.progressive.entity.Student;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
-
+// @RestController
+// @RequestMapping("/student")
 public class StudentController {
-
+    //   @GetMapping
     public ResponseEntity<List<Student>> getAllStudents() {
         return null;
     }
-
+    // @GetMapping("/{studentId}")
     public ResponseEntity<Student> getStudentById(int studentId) {
         return null;
     }
-
+    // @PostMapping
     public ResponseEntity<Integer> addStudent(Student student) {
         return null;
     }
-
+    // @PutMapping("/{studentId}")
     public ResponseEntity<Void> updateStudent(int studentId, Student student) {
         return null;
     }
-
+    //  @DeleteMapping("/{studentId}")
     public ResponseEntity<Void> deleteStudent(int studentId) {
         return null;
     }
-
+    // @GetMapping("/fromArrayList")
     public ResponseEntity<List<Student>> getAllStudentFromArrayList() {
         return null;
     }
-
+    // @PostMapping("/toArrayList")
     public ResponseEntity<Integer> addStudentToArrayList(Student student) {
         return null;
     }
-
+    // @GetMapping("/fromArrayList/sorted")
     public ResponseEntity<List<Student>> getAllStudentSortedByNameFromArrayList() {
         return null;
     }
--- server/src/main/java/com/edutech/progressive/service/impl/StudentServiceImplJpa.java
+++ server/src/main/java/com/edutech/progressive/service/impl/StudentServiceImplJpa.java
@@ -1,5 +1,22 @@
 package com.edutech.progressive.service.impl;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import com.edutech.progressive.entity.Student;
+
 public class StudentServiceImplJpa  {
+
+    // public List<Student> getAllStudents() throws Exception{
+    //     return new ArrayList<>();
+    // }
+
+    // public Integer addStudent(Student student) throws Exception{
+    //     return -1;
+    // }
+
+    // public List<Student> getAllStudentSortedByName() throws Exception{
+        
+    // }
 
 }
