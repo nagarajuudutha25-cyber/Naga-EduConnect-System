20f161a7edbf4438ab8e21ce473a2a9ef16bc266
nagarajuudutha25-cyber <dummy@gmail.com>
2026-01-28 17:44:29 +0000

    day10

--- server/src/main/java/com/edutech/progressive/repository/EnrollmentRepository.java
+++ server/src/main/java/com/edutech/progressive/repository/EnrollmentRepository.java
@@ -1,5 +1,62 @@
 package com.edutech.progressive.repository;
 
 
-public interface EnrollmentRepository {
+import java.util.List;
+
+import java.util.Optional;
+
+
+import javax.transaction.Transactional;
+
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import org.springframework.data.jpa.repository.Modifying;
+
+import org.springframework.data.jpa.repository.Query;
+
+import org.springframework.data.repository.query.Param;
+
+
+import com.edutech.progressive.entity.Enrollment;
+
+
+public interface EnrollmentRepository extends JpaRepository<Enrollment,Integer>{
+
+
+Optional<Enrollment> findByStudent_StudentIdAndCourse_CourseId(int studentId, int courseId);
+
+
+List<Enrollment> findAllByStudent_StudentId(int studentId);
+
+
+List<Enrollment> findAllByCourse_CourseId(int courseId);
+
+
+@Transactional
+
+@Modifying
+
+@Query("DELETE FROM Enrollment e WHERE e.course.courseId = :courseId")
+
+void deleteByCourseId(int courseId);
+
+
+@Transactional
+
+@Modifying
+
+@Query("DELETE FROM Enrollment e WHERE e.student.studentId = :studentId")
+
+void deleteByStudentId(int studentId);
+
+
+@Transactional
+
+@Modifying
+
+@Query("DELETE FROM Enrollment e WHERE e.course.courseId IN (SELECT c.courseId FROM Course c WHERE c.teacher.teacherId = :teacherId)")
+
+void deleteByTeacherId(@Param("teacherId") int teacherId);
+
 }
--- server/src/main/java/com/edutech/progressive/service/impl/EnrollmentServiceImpl.java
+++ server/src/main/java/com/edutech/progressive/service/impl/EnrollmentServiceImpl.java
@@ -1,5 +1,64 @@
 package com.edutech.progressive.service.impl;
 
-public class EnrollmentServiceImpl  {
+import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.edutech.progressive.entity.Enrollment;
+import com.edutech.progressive.repository.EnrollmentRepository;
+import com.edutech.progressive.service.EnrollmentService;
+
+@Service
+public class EnrollmentServiceImpl implements EnrollmentService {
+    
+    @Autowired
+    private EnrollmentRepository enrollmentRepository;
+
+    @Override
+    public List<Enrollment> getAllEnrollments() {
+        return enrollmentRepository.findAll();
+    }
+
+    @Override
+    public int createEnrollment(Enrollment enrollment) {
+        int studentId = enrollment.getStudent().getStudentId();
+        int courseId = enrollment.getCourse().getCourseId();
+
+        if (enrollmentRepository.findByStudent_StudentIdAndCourse_CourseId(studentId, courseId).isPresent()) {
+            throw new RuntimeException("Student is already enrolled in this course.");
+        }
+
+        //enrollment.setEnrollmentDate(new Date());
+        return enrollmentRepository.save(enrollment).getEnrollmentId();
+    }
+
+    @Override
+    public void updateEnrollment(Enrollment updatedEnrollment) {
+        Enrollment existingEnrollment = enrollmentRepository.findById(updatedEnrollment.getEnrollmentId())
+                .orElseThrow(() -> new RuntimeException("Enrollment not found with ID: " + updatedEnrollment.getEnrollmentId()));
+
+        existingEnrollment.setStudent(updatedEnrollment.getStudent());
+        existingEnrollment.setCourse(updatedEnrollment.getCourse());
+        //existingEnrollment.setEnrollmentDate(new Date());
+
+        enrollmentRepository.save(existingEnrollment);
+    }
+
+    @Override
+    public Enrollment getEnrollmentById(int enrollmentId) {
+        return enrollmentRepository.findById(enrollmentId)
+                .orElseThrow(() -> new RuntimeException("Enrollment not found with ID: " + enrollmentId));
+    }
+
+    @Override
+    public List<Enrollment> getAllEnrollmentsByStudent(int studentId) {
+        return enrollmentRepository.findAllByStudent_StudentId(studentId);
+    }
+
+    @Override
+    public List<Enrollment> getAllEnrollmentsByCourse(int courseId) {
+        return enrollmentRepository.findAllByCourse_CourseId(courseId);
+    }
 }
+
