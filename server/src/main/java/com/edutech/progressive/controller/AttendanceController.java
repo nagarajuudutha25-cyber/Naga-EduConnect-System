da100565b1630c5a952a07912b5b7cc32e958f75
nagarajuudutha25-cyber <dummy@gmail.com>
2026-01-28 19:58:35 +0000

    solve initial commit

--- server/src/main/java/com/edutech/progressive/controller/AttendanceController.java
+++ server/src/main/java/com/edutech/progressive/controller/AttendanceController.java
@@ -1,29 +1,76 @@
 package com.edutech.progressive.controller;
 
-import com.edutech.progressive.entity.Attendance;
-import org.springframework.http.ResponseEntity;
-
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.edutech.progressive.entity.Attendance;
+import com.edutech.progressive.service.impl.AttendanceServiceImpl;
+
+@RestController
+@RequestMapping("/attendance")
 public class AttendanceController {
 
+    @Autowired
+    AttendanceServiceImpl attendanceService;
+
+    @GetMapping
     public ResponseEntity<List<Attendance>> getAllAttendance() {
-        return null;
+        try {
+            List<Attendance> attendanceList = attendanceService.getAllAttendance();
+            return new ResponseEntity<>(attendanceList, HttpStatus.OK);
+        } catch (Exception e) {
+            return new ResponseEntity<>(null, HttpStatus.INTERNAL_SERVER_ERROR);
+        }
     }
 
-    public ResponseEntity<Attendance> createAttendance(Attendance attendance) {
-        return null;
+    @PostMapping
+    public ResponseEntity<Attendance> createAttendance(@RequestBody Attendance attendance) {
+        try {
+            Attendance attendanceSaved = attendanceService.createAttendance(attendance);
+            return new ResponseEntity<>(attendanceSaved, HttpStatus.CREATED);
+        } catch (Exception e) {
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
     }
 
-    public ResponseEntity<Integer> deleteAttendance(int attendanceId) {
-        return null;
+    @DeleteMapping("/{attendanceId}")
+    public ResponseEntity<Integer> deleteAttendance(@PathVariable int attendanceId) {
+        try {
+            attendanceService.deleteAttendance(attendanceId);
+            return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+        } catch (Exception e) {
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
     }
 
-    public ResponseEntity<List<Attendance>> getAllAttendanceByStudent(int studentId) {
-        return null;
+    @GetMapping("/student/{studentId}")
+    public ResponseEntity<List<Attendance>> getAllAttendanceByStudent(@PathVariable int studentId) {
+        try {
+            List<Attendance> attendanceList = attendanceService.getAttendanceByStudent(studentId);
+            return new ResponseEntity<>(attendanceList, HttpStatus.OK);
+        } catch (Exception e) {
+            return new ResponseEntity<>(null, HttpStatus.INTERNAL_SERVER_ERROR);
+        }
     }
 
-    public ResponseEntity<List<Attendance>> getAllAttendanceByCourse(int courseId) {
-        return null;
+    @GetMapping("/course/{courseId}")
+    public ResponseEntity<List<Attendance>> getAllAttendanceByCourse(@PathVariable int courseId) {
+        try {
+            List<Attendance> attendanceList = attendanceService.getAttendanceByCourse(courseId);
+            return new ResponseEntity<>(attendanceList, HttpStatus.OK);
+        } catch (Exception e) {
+            return new ResponseEntity<>(null, HttpStatus.INTERNAL_SERVER_ERROR);
+        }
     }
 }
+
--- server/src/main/java/com/edutech/progressive/service/impl/AttendanceServiceImpl.java
+++ server/src/main/java/com/edutech/progressive/service/impl/AttendanceServiceImpl.java
@@ -1,5 +1,51 @@
 package com.edutech.progressive.service.impl;
 
-public class AttendanceServiceImpl  {
+import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.edutech.progressive.entity.Attendance;
+import com.edutech.progressive.repository.AttendanceRepository;
+import com.edutech.progressive.service.AttendanceService;
+
+@Service
+public class AttendanceServiceImpl implements AttendanceService {
+
+    @Autowired
+    AttendanceRepository attendanceRepository;
+
+    @Override
+    public List<Attendance> getAllAttendance() {
+        return attendanceRepository.findAll();
+    }
+
+    @Override
+    public Attendance createAttendance(Attendance attendance) {
+        if (attendanceRepository.findByCourse_CourseIdAndStudent_StudentIdAndAttendanceDate(
+                attendance.getCourse().getCourseId(),
+                attendance.getStudent().getStudentId(),
+                attendance.getAttendanceDate()).isPresent()) {
+            throw new RuntimeException("Attendance for this student and course on the given date already exists.");
+        }
+        return attendanceRepository.save(attendance);
+    }
+
+    @Override
+    public void deleteAttendance(int attendanceId) {
+        if (!attendanceRepository.existsById(attendanceId)) {
+            throw new RuntimeException("Attendance record not found with ID: " + attendanceId);
+        }
+        attendanceRepository.deleteById(attendanceId);
+    }
+
+    @Override
+    public List<Attendance> getAttendanceByStudent(int studentId) {
+        return attendanceRepository.findByStudent_StudentId(studentId);
+    }
+
+    @Override
+    public List<Attendance> getAttendanceByCourse(int courseId) {
+        return attendanceRepository.findByCourse_CourseId(courseId);
+    }
 }
